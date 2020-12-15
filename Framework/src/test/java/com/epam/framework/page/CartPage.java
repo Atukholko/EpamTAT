08d32ab8dd04b343dbd0be29ae595b4ad25d38a0
Alexey Tukholko <a.tukholko@gmail.com>
2020-12-15 16:32:13 +0300

    Cart page modified

--- Framework/src/test/java/com/epam/framework/page/CartPage.java
+++ Framework/src/test/java/com/epam/framework/page/CartPage.java
@@ -3,10 +3,7 @@ package com.epam.framework.page;
 import com.epam.framework.wait.CustomWaits;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.openqa.selenium.By;
-import org.openqa.selenium.TimeoutException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -31,6 +28,24 @@ public class CartPage extends AbstractPage{
     @FindBy(className = "promo__oldprice")
     private WebElement oldPrice;
 
+    @FindBy(id = "j-cost-4115168")
+    private WebElement price;
+
+    @FindBy(xpath = "//*[text()='Оформить заказ']")
+    private WebElement checkout;
+
+    @FindBy(xpath = "//*[text()='Закрыть']")
+    private WebElement closeDialog;
+
+    @FindBy(id = "j-prepayment")
+    private WebElement certificateInput;
+
+    @FindBy(xpath = "//*[@class='order_total']//*[@class='g-price']")
+    private WebElement orderTotal;
+
+    private By cardInstallmentLocator = By.id("card_installment");
+    private By creditLocator = By.id("credit");
+    private By certificateLocator = By.id("sertificate");
     private By deleteFromCartButtonLocator = By.id("j-delete-5416247");
 
     public CartPage(WebDriver driver) {
@@ -52,6 +67,7 @@ public class CartPage extends AbstractPage{
         CustomWaits.waitForPageLoaded(driver);
         return this;
     }
+
     public CartPage applyPromoCode(){
         applyPromoCodeButton.click();
         return this;
@@ -67,6 +83,16 @@ public class CartPage extends AbstractPage{
         return oldPrice.getText();
     }
 
+    public String getPrice(){
+        new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS).until((WebDriver driver) -> !price.getText().equals(""));
+        return price.getText();
+    }
+
+    public String getOrderTotal(){
+        new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS).until((WebDriver driver) -> !orderTotal.getText().equals(""));
+        return orderTotal.getText();
+    }
+
     public Boolean isProductOnCart(String productCode){
         By productLocator = By.xpath("//*[@id=\"j-basket__items\"]//*[text()='код " + productCode + "']");
         try{
@@ -77,6 +103,47 @@ public class CartPage extends AbstractPage{
             return false;
         }
         return true;
+    }
+
+    public CartPage checkout(){
+        checkout.click();
+        CustomWaits.waitForPageLoaded(driver);
+        return this;
+    }
+
+    public CartPage chooseCardInstallment(){
+        WebElement cardInstallment = new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS)
+                .until(ExpectedConditions.presenceOfElementLocated(cardInstallmentLocator));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click()", cardInstallment);
+        return this;
+    }
+
+    public CartPage choosePaymentInParts(){
+        WebElement credit = new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS)
+                .until(ExpectedConditions.presenceOfElementLocated(creditLocator));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click()", credit);
+        return this;
+    }
+
+    public CartPage chooseCertificate(){
+        WebElement certificate = new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS)
+                .until(ExpectedConditions.presenceOfElementLocated(certificateLocator));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click()", certificate);
+        return this;
+    }
+
+    public CartPage inputCertificate(String value){
+        new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS)
+                .until(ExpectedConditions.elementToBeClickable(certificateInput));
+        certificateInput.sendKeys(value + "\n");
+        CustomWaits.waitForPageLoaded(driver);
+        return this;
+    }
+
+    public CartPage closeDialog(){
+        CustomWaits.waitForPageLoaded(driver);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click()", closeDialog);
+        return this;
     }
 
     @Override
